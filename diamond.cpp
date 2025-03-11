d5a3af5846fbefa9be6998a591aa75ecfba19e6a
amdg <adcpetechsoft@hotmail.com>
2025-03-11 17:19:10 +0800

    finishing

--- diamond.cpp
+++ diamond.cpp
@@ -1,13 +1,95 @@
-#define DSIZE 9
+#define DSIZE_MAX 9
 
-#include <iostream>;
+#include <iostream>
 
 void diamond_make(int diamond_size)
 {
+    using namespace std;
+
     int horizontal;
     int vertical;
+    int maxsize;
 
-    
+    int x;
+    int c;
+
+    horizontal=1;
+
+    maxsize=DSIZE_MAX;
+
+    while(horizontal<=diamond_size)
+    {
+        vertical=1;
+        x=maxsize-horizontal;
+
+        c=1;
+
+        while(vertical<=maxsize)
+        {
+            if(vertical>x)
+            {
+                cout<<c;
+                c++;
+            }else
+            {
+                cout<<" ";
+            };
+            vertical ++;
+        };
+
+        c=c-2;
+        while(c>0)
+        {
+            cout<<c;
+            c--;
+        };
+
+        cout<<endl;
+
+        horizontal++;
+
+    };
+
+    horizontal--;
+    horizontal--;
+
+
+    while(horizontal>0)
+    {
+        vertical=1;
+        x=maxsize-horizontal;
+
+        c=1;
+
+        while(vertical<=maxsize)
+        {
+            if(vertical>x)
+            {
+                cout<<c;
+                c++;
+            }else
+            {
+                cout<<" ";
+            };
+            vertical ++;
+        };
+
+        c=c-2;
+        while(c>0)
+        {
+            cout<<c;
+            c--;
+        };
+
+        cout<<endl;
+
+        horizontal--;
+
+    };
+
+
+
+
 
 
 };
@@ -15,11 +97,15 @@ void diamond_make(int diamond_size)
 
 int main(int argc, char *argv[])
 {
+    int i;
 
     using namespace std;
 
-    
-   
+    for(i=1; i<=9; i++)
+    {
+        diamond_make(i);
+        cout<<endl;
+    };
 
     return 0;
 };
