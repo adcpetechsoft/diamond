bf037e70e149310e386416f2bc9c8f1ff702b6e5
amdg <adcpetechsoft@hotmail.com>
2025-03-11 15:50:17 +0800

    initial_make_01

---
+++ diamond.cpp
@@ -0,0 +1,26 @@
+#define DSIZE 9
+
+#include <iostream>;
+
+void diamond_make(int diamond_size)
+{
+    int horizontal;
+    int vertical;
+
+    
+
+
+};
+
+
+int main(int argc, char *argv[])
+{
+
+    using namespace std;
+
+    
+   
+
+    return 0;
+};
+
