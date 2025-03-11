38f0c49b8a2b322e8514fffee2eefcd87c0f1104
amdg <adcpetechsoft@hotmail.com>
2025-03-11 18:12:42 +0800

    final_program

--- diamond.cpp
+++ diamond.cpp
@@ -1,9 +1,23 @@
+/*
+    Diamond Display Progrma
+    As a submission to https://code.golf/diamonds#cpp
+
+    Created by: amd
+*/
+
+// ---------------------------------
+// Preprocessor Declaration
 #define DSIZE_MAX 9
 
 #include <iostream>
 
+// ---------------------------------
+// Function diamond_make declaration
+// use to process and create display
 void diamond_make(int diamond_size)
 {
+    // ***********************
+    // Variable Declaration
     using namespace std;
 
     int horizontal;
@@ -13,17 +27,24 @@ void diamond_make(int diamond_size)
     int x;
     int c;
 
+    // ***********************
+    // Actual Function
+
+    // pre horizontal count and initialization
     horizontal=1;
 
     maxsize=DSIZE_MAX;
 
+    // looping for top horizontal precess and print
     while(horizontal<=diamond_size)
     {
+        // vertical variable pre value
         vertical=1;
         x=maxsize-horizontal;
 
         c=1;
 
+        // looping for lestside line process & prints
         while(vertical<=maxsize)
         {
             if(vertical>x)
@@ -38,6 +59,8 @@ void diamond_make(int diamond_size)
            vertical ++;
         };
 
+        // looping for right side process and print
+
         c=c-2;
         while(c>0)
         {
@@ -51,17 +74,21 @@ void diamond_make(int diamond_size)
 
     };
 
+    // variables process for bottom horizontal
     horizontal--;
     horizontal--;
 
-
+    // loopoing for bottom horizontal process and print
     while(horizontal>0)
     {
+        // vertical variable pre value
         vertical=1;
         x=maxsize-horizontal;
 
         c=1;
 
+
+        // looping for lestside line process & prints
         while(vertical<=maxsize)
         {
             if(vertical>x)
@@ -75,6 +102,7 @@ void diamond_make(int diamond_size)
             vertical ++;
         };
 
+        // looping for lestside line process & prints
         c=c-2;
         while(c>0)
         {
@@ -90,13 +118,20 @@ void diamond_make(int diamond_size)
 
 };
 
-
+// ---------------------------------
+// Progran  Entry Point
 int main(int argc, char *argv[])
 {
+    // ***********************
+    // Variable Declaration
     int i;
 
+
+    // ***********************
+    // Actual Procedure
     using namespace std;
 
+    // loop for multiple display
     for(i=1; i<=DSIZE_MAX; i++)
     {
         diamond_make(i);
